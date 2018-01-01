4e31f6257379485e08d95b63cd75c52763145bca
Paturages <christian.ingouff@saegus.com>
2018-01-01 13:48:40 +0100

    Add schmutz

---
+++ sources/discographies/schmutz.js
@@ -0,0 +1,9 @@
+module.exports = {
+  driveUrl: 'https://drive.google.com/drive/folders/1jm5uKmaDXCeGK9jWLVWykeq_ZLHcGyuC',
+  driveName: 'Schmutz06 CH Conversions',
+  charterName: 'Schmutz06',
+  nameParser: name => ({
+    artist: 'Schmutz06',
+    song: name
+  })
+}
