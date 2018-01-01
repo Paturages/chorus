7578fe71329b5cf0150d8746ea1efad715a5f562
Paturages <christian.ingouff@saegus.com>
2018-01-01 17:09:43 +0100

    Add all GH/RB games!

--- sources/packs/house-of-customs.js
+++ sources/packs/house-of-customs.js
@@ -1,4 +1,5 @@
 module.exports = {
   driveUrl: 'https://drive.google.com/drive/folders/1INT7iV0Yr9sAy1c9vVrFhWxyTRevOUyc',
-  driveName: 'House of Customs'
+  driveName: 'House of Customs',
+  charterName: 'Farrotone',
 }
