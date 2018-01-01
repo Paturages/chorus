6c6156d3984c3a9b84a87fb2530dd9752c2bec9e
Paturages <christian.ingouff@saegus.com>
2018-01-01 16:01:15 +0100

    Add more drives (thanks @sandsoles16)

---
+++ sources/ghrb/rb4.js
@@ -0,0 +1,5 @@
+module.exports = {
+  driveUrl: 'https://drive.google.com/drive/folders/1caIINiErpzzseupFcAeytnvMqFmgGVhC',
+  driveName: 'Rock Band 4 (Recreations)',
+  charterName: 'Harmonix'
+}
---
+++ sources/packs/house-of-customs.js
@@ -0,0 +1,4 @@
+module.exports = {
+  driveUrl: 'https://drive.google.com/drive/folders/1INT7iV0Yr9sAy1c9vVrFhWxyTRevOUyc',
+  driveName: 'House of Customs'
+}
