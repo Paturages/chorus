3309249f1804bec62852e43169772ffa28b047c8
Tetrinity <zephyrsg@hotmail.co.uk>
2018-01-01 22:30:37 +0000

    Added Google Drive for Tetrinity

---
+++ sources/tetrinity.js
@@ -0,0 +1,5 @@
+module.exports = {
+  driveUrl: 'https://drive.google.com/drive/folders/1IFplFLiKX8mxnnfmvCTIEqBb_1r9i-rb',
+  driveName: "Tetrinity's Clone Hero charts",
+  charterName: 'Tetrinity'
+};
