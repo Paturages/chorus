59024e9455a2fb874ad91cc752e7ed61422535d1
Paturages <christian.ingouff@saegus.com>
2018-01-03 20:24:49 +0100

    Add credit for bill wurtz

--- sources/discographies/bill-wurtz.js
+++ sources/discographies/bill-wurtz.js
@@ -1,5 +1,6 @@
 module.exports = {
   driveUrl: 'https://drive.google.com/drive/folders/111-eq6IdjKkvQXv0dkNsg_umsqMziPDU',
   driveName: 'bill wurtz',
+  charterName: 'RobGHH',
   nameParser: name => (name.slice(-4) == '.ini' ? {} : { song: name, artist: 'bill wurtz' })
 }
