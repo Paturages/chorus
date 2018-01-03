ede5ae32b408e999cf11c8bb42d472c16919abbb
Paturages <Paturages@users.noreply.github.com>
2018-01-03 14:05:21 +0100

    Fix Puppetz Hero 5 link

--- sources/packs/puppetz-hero-5.js
+++ sources/packs/puppetz-hero-5.js
@@ -1,4 +1,4 @@
 module.exports = {
-  driveUrl: 'https://drive.google.com/open?id=10Y2Y76Bsky9yN5Ggv20ecHBD8dGlleZy',
+  driveUrl: 'https://drive.google.com/drive/folders/10Y2Y76Bsky9yN5Ggv20ecHBD8dGlleZy',
   driveName: 'Puppetz Hero V'
 };
