986cb036fc3f970f552777f32939e18669ebf41a
Paturages <christian.ingouff@saegus.com>
2018-01-03 20:17:42 +0100

    Add more packs

---
+++ sources/discographies/bill-wurtz.js
@@ -0,0 +1,5 @@
+module.exports = {
+  driveUrl: 'https://drive.google.com/drive/folders/111-eq6IdjKkvQXv0dkNsg_umsqMziPDU',
+  driveName: 'bill wurtz',
+  nameParser: name => (name.slice(-4) == '.ini' ? {} : { song: name, artist: 'bill wurtz' })
+}
---
+++ sources/packs/overchart.js
@@ -0,0 +1,4 @@
+module.exports = {
+  driveUrl: 'https://drive.google.com/drive/folders/1rfpaYmO5_fXNxG3TxtMkdrVYeob4O1b1',
+  driveName: 'Overchart Setlist'
+};
