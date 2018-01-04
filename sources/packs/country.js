889fa40cb4ec1fe3f68316e869704836b7c409a8
Paturages <christian.ingouff@saegus.com>
2018-01-04 19:27:49 +0100

    Add rap/hip-hop and country packs from the spreadsheet

---
+++ sources/packs/country.js
@@ -0,0 +1,4 @@
+module.exports = {
+  driveUrl: 'https://drive.google.com/drive/folders/1hmdPoZK629Qv8ixPr6DwDWQlKPcf1oDA',
+  driveName: 'Country Pack'
+};
---
+++ sources/packs/rap-hiphop.js
@@ -0,0 +1,4 @@
+module.exports = {
+  driveUrl: 'https://drive.google.com/drive/folders/12sQo6ghHBXeXDE3iN3k8CNNmjprwVKUM',
+  driveName: 'Rap & Hip-hop Pack'
+};
