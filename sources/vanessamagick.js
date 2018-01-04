518a5b73c93679672364be9ca93d4a423924d8ff
VanessaMagick <35085611+VanessaMagick@users.noreply.github.com>
2018-01-04 12:46:02 +1100

    Create vanessamagick.js

---
+++ sources/vanessamagick.js
@@ -0,0 +1,5 @@
+module.exports = {
+  driveUrl: 'https://drive.google.com/drive/u/2/folders/1rC9loeBoowMX5c91WZNMgLAZlkdQ6qU9',
+  driveName: `VanessaMagick's Charts`,
+   charterName: `VanessaMagick`
+};
