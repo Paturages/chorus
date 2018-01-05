589a51ca40b302d5e38de01d0c2e104c7b39a271
Paturages <christian.ingouff@saegus.com>
2018-01-05 09:57:23 +0100

    Normalize Tsuunaamii's drive link

--- sources/Tsuunaamii.js
+++ sources/Tsuunaamii.js
@@ -1,5 +1,5 @@
 module.exports = {
-  driveUrl: 'https://drive.google.com/open?id=1fQhmu3nB4t2SQiPIRzXazxs6HbHXogT8',
+  driveUrl: 'https://drive.google.com/drive/folders/1fQhmu3nB4t2SQiPIRzXazxs6HbHXogT8',
   driveName: `Tsuunaamii's Charts`,
   charterName: `Tsuunaamii`
 };
