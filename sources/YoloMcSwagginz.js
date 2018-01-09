35ef028fe92293f2c0556ca4cac555d9a0434983
Paturages <c.ingouff@gmail.com>
2018-01-09 15:37:09 +0100

    Normalize YoloMcSwagginz's drive URL

---
+++ sources/YoloMcSwagginz.js
@@ -0,0 +1,5 @@
+module.exports = {
+  driveUrl: 'https://drive.google.com/drive/folders/12F0GfMIro8jlJEmXoad9jFl08S49Hkqo',
+  driveName: `YoloMcSwagginz' charts`,
+  charterName: `YoloMcSwagginz`
+};
