d71e61101e564ad5269261f0d1eea9784144aebc
Paturages <Paturages@users.noreply.github.com>
2022-01-24 20:51:37 +0100

    dunk ganalytics in the proverbial garbage can
    well, besides tracking concerns and mayyyyyyyybe gdpr I don't really give a fuck about metrics anymore lol

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -27,61 +27,6 @@ if (bg) {
   document.getElementById('background').style.backgroundImage = `url(${bg})`;
 }
 
-// Evil good ol' spying Google Analytics >:)
-if (process.env.NODE_ENV === 'production' && !process.env.TESTING) {
-  (function (i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    (i[r] =
-      i[r] ||
-      function () {
-        (i[r].q = i[r].q || []).push(arguments);
-      }),
-      (i[r].l = 1 * new Date());
-    (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m);
-  })(
-    window,
-    document,
-    'script',
-    'https://www.google-analytics.com/analytics.js',
-    'ga'
-  );
-  ga('create', 'UA-112049887-1', 'auto');
-  ga('send', 'pageview');
-}
-
-/*
-
---- TODO: Uncomment when feeling like implementing Discord auth ---
-
-const hashParts = (window.location.hash || "")
-  .slice(1)
-  .split("&")
-  .map(x => {
-    const [key, value] = x.split("=");
-    return { key, value };
-  });
-// Catch Discord OAuth2 redirect
-let accessToken = hashParts.find(x => x.key == "access_token");
-if (accessToken) window.localStorage.setItem("accessToken", accessToken.value);
-else accessToken = { value: window.localStorage.getItem("accessToken") };
-
-accessToken.value &&
-  Http.get(
-    "https://discordapp.com/api/guilds/296481029303304192/members",
-    null,
-    {
-      Authorization: `Bearer ${accessToken.value}`,
-      "Content-Type": "application/x-www-form-urlencoded"
-    }
-  ).then(guilds => {
-    console.log(guilds);
-  });
-
-*/
-
 class ChorusApp extends Component {
   constructor(props) {
     super(props);
